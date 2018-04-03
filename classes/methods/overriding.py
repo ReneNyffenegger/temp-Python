f821e30ec375c191acc3bf7889c50d523338fb91
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-04-03 14:18:39 +0200

    Overriding methods etc

---
+++ classes/methods/overriding.py
@@ -0,0 +1,24 @@
+class A:
+
+   def __eq__(self, o):
+       print('xxx')
+       return False
+
+
+class B:
+    def p(self): # A method is a function that takes a class instance as its first parameter. 
+        print('I am a A')
+
+
+a = A()
+b = B()
+
+if a is b:
+   print ('a is b')
+else:
+   print ('a is not b')
+
+b.p()
+
+if a == b:
+   print ('a is b')
