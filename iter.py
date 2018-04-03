d3ac5e92c227c8046cf6ef0a091497dd8f95c428
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-04-03 14:31:13 +0200

    + iter.py

---
+++ iter.py
@@ -0,0 +1,32 @@
+
+prims_list = [2, 3, 5, 7, 11, 13, 17]
+prims_list_i = iter(prims_list)
+print(next(prims_list_i))
+print(next(prims_list_i))
+print(next(prims_list_i))
+print(next(prims_list_i))
+print(next(prims_list_i))
+print(next(prims_list_i))
+print(next(prims_list_i))
+
+class PRIMS:
+    def __init__(self):
+        print('__init__')
+        self.prims=[2, 3, 5, 7, 11, 13, 17]
+
+    def __iter__(self):
+        self.cur = -1
+        return self
+
+    def __next__(self):
+        if self.cur + 1 >= len(self.prims):
+           raise StopIteration
+        self.cur += 1
+        return self.prims[self.cur]
+        
+        
+
+prims = PRIMS()
+p_i = iter(prims)
+for p_ in p_i:
+      print('p_ = ' + str(p_))
