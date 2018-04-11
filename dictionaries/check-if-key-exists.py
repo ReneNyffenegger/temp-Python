1f58edadd11f816fb08019bc8d5da3167fc725f0
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-04-11 17:25:36 +0200

    + check-if-key-exists.py

---
+++ dictionaries/check-if-key-exists.py
@@ -0,0 +1,13 @@
+d = {'x': 'eggs',
+     'y': 'why',
+     'z':  42}
+
+if 'x' in d:
+    print('key x exists in d')
+else:
+    print('key x does not exist in d')
+
+if 'a' in d:
+    print('key a exists in d')
+else:
+    print('key a does not exist in d')
