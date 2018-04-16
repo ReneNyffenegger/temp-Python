afc5385d78134296236e845ef64b1933206f58da
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-04-16 09:21:06 +0200

    false local variable

--- variables/local-global-func.py
+++ variables/local-global-func.py
@@ -1,8 +1,22 @@
 g_one = 42
+g_two = 99
 
 def G():
+  #
+  # This function has no assignment to g_one,
+  # therefore, this variable is recognized as global.
+  #
     v = g_one
     v = v+1
     print('v = {:d}'.format(v))
+  #
+  # However, there is an assignement to g_two,
+  # the variable is falsely assumed to be local!
+  #
+    try:
+      print(g_two)
+      g_two += 1
+    except UnboundLocalError as e:
+      print(str(e))
 
 G()
