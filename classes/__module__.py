a4201c6908a7ecbb38bc6b0e87013978533e8a67
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-05-07 10:57:58 +0200

    + __module__.py

---
+++ classes/__module__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+class ABCD:
+      pass
+
+class EFGH:
+      pass
+
+abcd = ABCD()
+efgh = EFGH()
+
+#
+# Every Python class has a built-in class attribute __module__.
+# It stores the name of the module in which the class is defined. 
+#
+print ('abcd.__module__ = {:s}'.format(abcd.__module__)) # __main__
+print ('efgh.__module__ = {:s}'.format(efgh.__module__)) # __main__
