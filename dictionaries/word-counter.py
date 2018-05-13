beaeba0c10f79de4228765932b982c5a7719e5d6
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-05-13 21:42:56 +0200

    Use collections.Counter

--- dictionaries/word-counter.py
+++ dictionaries/word-counter.py
@@ -1,10 +1,28 @@
 #!/usr/bin/python
 
-word_counter = {}
+from collections import Counter
 
-for word in ['foo', 'bar', 'foo', 'foo', 'bar', 'baz', 'baz', 'foo', 'bar']:
-    word_counter[word] = word_counter.get(word, 0) + 1
 
-for word, cnt in word_counter.items():
-    print('{:2d} x {:s}'.format(cnt, word))
+words = ['foo', 'bar', 'foo', 'foo', 'bar', 'baz', 'baz', 'foo', 'bar']
 
+def show_dict(d):
+    for word, cnt in d.items():
+        print('{:2d} x {:s}'.format(cnt, word))
+
+def count_with_dict():
+
+    word_counter = {}
+
+    for word in words:
+        word_counter[word] = word_counter.get(word, 0) + 1
+
+    show_dict(word_counter)
+    
+
+def count_with_Counter():
+    counted_words = Counter(words)
+    show_dict(counted_words)
+
+
+count_with_dict()
+count_with_Counter()
