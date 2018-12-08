aa06cae34c63b0af7b5174e96daf2004afbd9821
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-12-08 09:43:55 +0100

    Value errors

--- tuples/return-tuple.py
+++ tuples/return-tuple.py
@@ -8,3 +8,20 @@ one, two, three = tup_3()
 print (one  )
 print (two  )
 print (three)
+
+tup = tup_3()
+print (type(tup)) # <class 'tuple'>
+for i in tup:
+    print(i)
+
+print(tup[0])
+print(tup[1])
+print(tup[2])
+
+# Following causes
+#   ValueError: not enough values to unpack (expected 4, got 3)
+# a,b,c,d = tup_3()
+
+# Following causes
+#   ValueError: too many values to unpack (expected 2)
+# a,b = tup_3()
