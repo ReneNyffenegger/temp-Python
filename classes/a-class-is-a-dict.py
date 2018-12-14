45d143d109f9e5ead6eaf19f9278b01615bb0f78
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-12-14 16:05:21 +0100

    n/a

--- classes/a-class-is-a-dict.py
+++ classes/a-class-is-a-dict.py
@@ -1,11 +1,13 @@
-class Foo:
+#!/usr/bin/python3
+
+class C:
     def f_one(self):
         print('hello')
 
     def f_two(self):
         pass
 
-for k in Foo.__dict__:
+for k in C.__dict__:
     print('k = {:s}'.format(k))
 #
 # k = __module__
@@ -15,10 +17,17 @@ for k in Foo.__dict__:
 # k = __weakref__
 # k = __doc__
 
-foo = Foo()
+obj_1 = C()
+obj_2 = C()
+
+obj_2.x = 'eggs'
+obj_2.y = 'why'
 
 
 print ('-------------')
-for k in foo.__dict__:
+for k in obj_1.__dict__:
     print('k = {:s}'.format(k))
 
+print ('-------------')
+for k in obj_2.__dict__:
+    print('k = {:s}'.format(k))
