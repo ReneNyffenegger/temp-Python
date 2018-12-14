894bef9b9c76cd6f73314342e1259c678b126b31
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-12-15 00:49:21 +0100

    n/a

--- classes/__methods__.py
+++ classes/__methods__.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
 
-# __annotations__  A dict containing annotations of parameters. The keys of the dict are the parameter names, and 'return' for the return annotation, if provided.
-# __call__         Used for callables.
-# __call__         Used for callables.
-# __closure__      None or a tuple of cells that contain bindings for the function’s free variables. See below for information on the cell_contents attribute.
-# __code__         The code object representing the compiled function body.
-# __defaults__     A tuple containing default argument values for those arguments that have defaults, or None if no arguments have a default value
-# __dict__         The namespace supporting arbitrary function attribut
-# __doc__          The function’s documentation string, or None if unavailable; not inherited by subclasses
-# __func__         is the function object; 
-# __globals__      A reference to the dictionary that holds the function’s global variables — the global namespace of the module in which the function was defined.
-# __iter__
-# __kwdefaults__   A dict containing defaults for keyword-only parameters.
-# __metaclass__    this was introduced to give the programmer some control over the semantics of the class statement. In particular it eases the transition from old-style classes (which are not covered in this tutorial) and new-style classes (simply called classes in this tutorial).
-# __module__       The name of the module the function was defined in, or None if unavailable.
-# __name__         The function's name
-# __next__
-# __qualname__     The function’s qualified name
-# __self__         is the class instance object,
-# __slots__
+#            __annotations__  A dict containing annotations of parameters. The keys of the dict are the parameter names, and 'return' for the return annotation, if provided.
+# class.     __bases__        The tuple of base classes of a class object.
+#            __call__         Used for callables.
+# instance.  __class__        the class to which an instance belongs
+#            __closure__      None or a tuple of cells that contain bindings for the function’s free variables. See below for information on the cell_contents attribute.
+#            __code__         The code object representing the compiled function body.
+#            __defaults__     A tuple containing default argument values for those arguments that have defaults, or None if no arguments have a default value
+#            __dict__         The namespace supporting arbitrary function attribut
+#            __doc__          The function’s documentation string, or None if unavailable; not inherited by subclasses
+#            __func__         is the function object; 
+#            __globals__      A reference to the dictionary that holds the function’s global variables — the global namespace of the module in which the function was defined.
+#            __iter__
+#            __kwdefaults__   A dict containing defaults for keyword-only parameters.
+# class.     __mro__           a tuple of classes that are considered when looking for base classes during method resolution.
+#            __metaclass__    this was introduced to give the programmer some control over the semantics of the class statement. In particular it eases the transition from old-style classes (which are not covered in this tutorial) and new-style classes (simply called classes in this tutorial).
+#            __module__       The name of the module the function was defined in, or None if unavailable.
+# definition.__name__         The function's, class's, method's, descriptor's or generator's name 
+#            __next__
+#            __qualname__     The function’s qualified name
+#            __self__         is the class instance object,
+#            __slots__
+# class.     __subclasses__
 
