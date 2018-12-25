be3eef40ded1612617367c282e39647456af7338
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-12-25 16:43:02 +0100

    + getter-setter

---
+++ decorators/getter-setter.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+#
+#   https://stackoverflow.com/a/18583708/180275
+
+class C(object):
+    def __init__(self):
+        self._x = None
+
+    @property
+    def x(self):
+        print('  property x')
+        return self._x
+
+    @x.setter
+    def x(self, value):
+        print('  setter x')
+        self._x = value
+
+    @x.deleter
+    def x(self):
+        print('  deleter x')
+        del self._x
+
+
+
+c_1  = C()
+c_2  = C()
+
+print('Assigning one')
+c_1.x = 'one'
+
+print('Assigning two')
+c_2.x = 'two'
+
+print('Getting one')
+print('  ' + c_1.x)
+
+print('Getting two')
+print('  ' + c_2.x)
+
+del c_1.x
+print('  ' + c_1.x)
