ecacebbd83f23927b8c2dbaf3af24ee3136316fe
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-12-25 21:50:04 +0100

    + innerF.py

---
+++ functions/inner/innerF.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def F_outer():
+
+    def F_inner():
+        print('F_inner')
+
+    print('F_outer')
+    F_inner()
+
+F_outer()
+
+# 'function' object has no attribute 'F_inner'
+# F_outer.F_inner()
