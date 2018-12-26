c0b8f7d6c23a4f7658f2e277d69edd2d2ce4c399
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-12-26 08:59:35 +0100

    n/a

--- literals/f.py
+++ literals/f.py
@@ -3,5 +3,15 @@
 x='eggs'
 y='hwy'
 num=42
+dbl=123.456789
 
-print(f"x: {x}, y: {y}, num: {num}")
+print(f"x: {x}, y: {y}, num: {num} dbl: {dbl}")
+
+print(f"x: {x}, y: {y}, num: {num} dbl: {dbl:.4f}")
+print(f"x: {x}, y: {y}, num: {num} dbl: {dbl:7.1f}")
+
+def Func():
+    pass
+
+print(f"name of func: {Func.__name__}")
+print(f"name of func: {Func.__name__!r}")  # !r: use repr()
