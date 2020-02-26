83d867ff5de259ddf54493e146320b18a7e13031
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-02-26 09:34:59 +0100

    n/a

--- iterators/iter.py
+++ iterators/iter.py
@@ -14,16 +14,18 @@ print(next(prims_list_i))
 print(next(prims_list_i))
 print(next(prims_list_i))
 
-class PRIMS:
+class PRIMES:
     def __init__(self):
         print('__init__')
         self.prims=[2, 3, 5, 7, 11, 13, 17]
 
     def __iter__(self):
+        print('__iter__')
         self.cur = -1
         return self
 
     def __next__(self):
+        print('__next__')
         if self.cur + 1 >= len(self.prims):
            raise StopIteration
         self.cur += 1
@@ -31,7 +33,21 @@ class PRIMS:
         
         
 
-prims = PRIMS()
-p_i = iter(prims)
+primes = PRIMES()
+p_i = iter(primes)
 for p_ in p_i:
       print('p_ = ' + str(p_))
+
+primes_again = PRIMES()
+for pa in primes_again:
+    print(pa)
+
+yet_again = PRIMES()
+iter(yet_again)
+
+try:
+   while True:
+         x = next(yet_again)
+         print(x)
+except StopIteration:
+   print('finished')
