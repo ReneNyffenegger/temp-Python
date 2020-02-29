e12e869720761c77f7deb9e03ec404ca21a8b6d5
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-02-29 01:48:29 +0100

    n/a

---
+++ built-in-functions/map/simple.py
@@ -0,0 +1,24 @@
+def f(i):
+    return i * 3
+
+l = [4,5,6]
+print(type(l)) # <class 'list'>
+
+print(l[1]) # 5
+
+m = map(f, l)
+
+print(type(m)) # <class 'map'>
+
+# print(m[0]) # TypeError: 'map' object is not subscriptable
+
+
+l = [6,7,8,9]
+
+for i in m:
+    print(i)
+
+
+m_ = map(lambda n: n*3, l)
+for i in m_:
+    print(i)
