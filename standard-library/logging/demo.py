45445efa68bbfd959dd36c37b689114fcf6cbee5
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-05-27 16:47:44 +0200

    + demo.py

---
+++ standard-library/logging/demo.py
@@ -0,0 +1,7 @@
+import logging
+
+logging.debug   ('A debug message.'   )
+logging.info    ('A info message.'    )
+logging.warning ('A warning message.' )
+logging.error   ('An error message.'  )
+logging.critical('A critical message.')
