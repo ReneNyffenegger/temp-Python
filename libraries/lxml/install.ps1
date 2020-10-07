fc54008052aec244ca52fd8a350e3447bfd1cfd1
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-10-07 10:41:41 +0200

    n/a

---
+++ libraries/lxml/install.ps1
@@ -0,0 +1,24 @@
+# where.exe pip
+# C:\Python39\Scripts\pip.exe
+
+#
+#  Run as admin ?
+#
+#  It seems to require
+#     Microsoft Visual C++ 14.0
+#   Get it with "Build Tools for Visual Studio at
+#     https://visualstudio.microsoft.com/downloads/
+
+#
+#   The following does not seem to work, either:
+#     pip install beautifulsoup4  lxml
+
+#  Can it be downloaded from
+#    https://www.lfd.uci.edu/~gohlke/pythonlibs/#lxml
+#
+#    For example
+#      lxml-4.5.2-cp39-cp39-win_amd64.whl
+     pip install C:\Users\Rene\Downloads\lxml-4.5.2-cp39-cp39-win_amd64.whl
+
+
+pip install  
