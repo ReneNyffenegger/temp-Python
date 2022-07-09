a7aae86da967c41eb7c0e0519b2383de83c4648a
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2022-07-09 22:12:17 +0200

    n/a

--- functions/open/classes.py
+++ functions/open/classes.py
@@ -3,11 +3,12 @@ wb = open('wb'    , 'wb')
 rb = open(__file__, 'rb')
 
 def cls(obj):
-    obj_t = type(obj)
-    print('{:20s}:  {:s}'.format(
-       '.'.join([obj_t.__module__, obj_t.__qualname__]),
-       ' <- '.join( map(lambda t: '.'.join([t.__module__, t.__qualname__]), type(obj).mro())
-    )))
+#   obj_t = type(obj)
+    print(' <- '.join( map(lambda t: '.'.join([t.__module__, t.__qualname__]), type(obj).mro())))
+#   print('{:20s}:  {:s}'.format(
+#      '.'.join([obj_t.__module__, obj_t.__qualname__]),
+#      ' <- '.join( map(lambda t: '.'.join([t.__module__, t.__qualname__]), type(obj).mro())
+#   )))
 
 
 cls(w_)
