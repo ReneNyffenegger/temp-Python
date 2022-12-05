7591f86d537875881d3f6ea2f7eb8f0b7a2d944e
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2022-12-05 08:34:14 +0100

    .

---
+++ misc/parse-CSV.py
@@ -0,0 +1,17 @@
+import re
+
+nofFields = 3
+reText = ';'.join(['([^;]+)'] * nofFields)
+print(reText)
+
+csvRe = re.compile( reText )
+if m := csvRe.match('foo;bar;baz'):
+   print(m[1])
+   print(m[2])
+   print(m[3])
+
+#while dataLine := data.readline():
+#     dataLine = dataLine.rstrip('\n')
+
+#if fields := csvRe.match(dataLine):
+#   print(fields[1])
