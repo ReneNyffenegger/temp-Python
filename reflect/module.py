b09af72851ad9cfd7a6f2890d4026c660604be66
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2023-02-24 22:08:26 +0100

    .

--- reflect/module.py
+++ reflect/module.py
@@ -3,5 +3,18 @@
 # import torch
 # obj = mechanicalsoup
 
-for member in [ _ for _ in sorted(dir(obj), key = lambda m: m.replace('_', '')) if not _.startswith('__') ]:
-    print(member + '\t' + str(type(getattr(obj, member))))
+for mem in [ _ for _ in sorted(dir(obj), key = lambda m: m.replace('_', '')) if not _.startswith('__') ]:
+
+    member = getattr(obj, mem)
+    typ = type(member)
+#   print(isinstance(typ, object))
+
+    if isinstance(member, type(lambda: 0)):
+       member = f'{mem}()'
+       type_  =  ''
+
+    else:
+       member = mem
+       type_  = str(typ)
+
+    print(f'`{member}` ☰  {type_}' )
