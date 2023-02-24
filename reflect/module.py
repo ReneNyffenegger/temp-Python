5c365ce1808521417f5afcbbd87ff29eb32f5669
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2023-02-24 20:12:49 +0100

    .

--- reflect/module.py
+++ reflect/module.py
@@ -1,4 +1,7 @@
-import torch
+# exec(open('/home/tq84/github/temp/Python/reflect/module.py').read(), {**globals(), "obj": mechanicalsoup})
 
-for member in sorted(dir(torch), key = lambda m: m.replace('_', '')):
-    print(member + '\t' + str(type(getattr(torch, member))))
+# import torch
+# obj = mechanicalsoup
+
+for member in [ _ for _ in sorted(dir(obj), key = lambda m: m.replace('_', '')) if not _.startswith('__') ]:
+    print(member + '\t' + str(type(getattr(obj, member))))
