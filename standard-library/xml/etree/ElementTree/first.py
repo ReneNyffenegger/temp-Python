922ab5c0e51c45b0549b393aaa226300017711f0
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2023-03-13 16:36:01 +0100

    .

---
+++ standard-library/xml/etree/ElementTree/first.py
@@ -0,0 +1,38 @@
+import xml.etree.ElementTree as ET
+
+# tree = ET.fromstring('''<?xml version="1.0"?>
+# <data>
+#     <country name="Liechtenstein">
+#         <rank>1</rank>
+#         <year>2008</year>
+#         <gdppc>141100</gdppc>
+#         <neighbor name="Austria" direction="E"/>
+#         <neighbor name="Switzerland" direction="W"/>
+#     </country>
+#     <country name="Singapore">
+#         <rank>4</rank>
+#         <year>2011</year>
+#         <gdppc>59900</gdppc>
+#         <neighbor name="Malaysia" direction="N"/>
+#     </country>
+#     <country name="Panama">
+#         <rank>68</rank>
+#         <year>2011</year>
+#         <gdppc>13600</gdppc>
+#         <neighbor name="Costa Rica" direction="W"/>
+#         <neighbor name="Colombia" direction="E"/>
+#     </country>
+# </data>
+# ''')
+
+
+tree = ET.parse('country_data.xml')
+# print(type(tree))
+tq84_reflect(tree)
+
+root = tree.getroot()
+
+
+
+
+#print(type(root))
