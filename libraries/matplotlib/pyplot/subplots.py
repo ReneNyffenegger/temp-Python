cde75c435e24c1cc4e12cc51d3ffc7e9aba1e598
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2023-03-17 21:12:18 +0100

    .

---
+++ libraries/matplotlib/pyplot/subplots.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots()  # Create a figure containing a single axes.
+ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Plot some data on the axes.
+
+fig.show()
+plt.waitforbuttonpress()
