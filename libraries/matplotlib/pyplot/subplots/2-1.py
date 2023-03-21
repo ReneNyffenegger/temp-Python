faf6b89e506cd25ed062dc001a1167ecde74f893
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2023-03-21 23:13:14 +0100

    .

--- libraries/matplotlib/pyplot/subplots/2-1.py
+++ libraries/matplotlib/pyplot/subplots/2-1.py
@@ -1,12 +1,15 @@
+#!/usr/bin/env python
+
 import matplotlib.pyplot as plt
 
-def example_plot(ax, fontsize=12):
+def example_plot(ax, faceColor):
     ax.plot([1, 2])
+    ax.set_facecolor(faceColor)
 
     ax.locator_params(nbins=3)
-    ax.set_xlabel('x-label', fontsize=fontsize)
-    ax.set_ylabel('y-label', fontsize=fontsize)
-    ax.set_title('Title', fontsize=fontsize)
+    ax.set_xlabel('x-label', fontsize=11)
+    ax.set_ylabel('y-label', fontsize=11)
+    ax.set_title('Title'   , fontsize=11)
 
 
 fig = plt.figure()
@@ -15,9 +18,9 @@ ax1 = plt.subplot(221)
 ax2 = plt.subplot(223)
 ax3 = plt.subplot(122)
 
-example_plot(ax1)
-example_plot(ax2)
-example_plot(ax3)
+example_plot(ax1, 'cornflowerblue')
+example_plot(ax2, 'palegreen'     )
+example_plot(ax3, 'khaki'         )
 
 plt.tight_layout()
 
