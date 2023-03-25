d7092fddb1902ad032813a74faf7ae244a38029f
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2023-03-25 22:14:59 +0100

    .

---
+++ libraries/GeographicLib/dist_from_great_circle_to_point.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+
+#
+# https://stackoverflow.com/a/51895595
+#
+
+__author__  = 'hrmd'
+
+from    geographiclib.geodesic  import  Geodesic
+import  numpy                   as      np
+
+def dist_from_great_circle_to_point(a, b, p):
+    '''
+    Calculates the shortest distance between a point P and a great
+    circle passing through A and B. The method is:
+        1. Find azimuth for line AP.
+        2. Find azimuth for line AB.
+        3. Consider point M which is the mirror image of point P
+            in the great circle. Find azimuth AM from the first
+            two azimuths.
+        4. Find great circle between P and M. The point of interest
+            is half-way along this line.
+
+    Input
+    a       [lon, lat] of first point defining the great circle.
+    b       [lon, lat] of second point defining the great circle.
+    p       [lon, lat] of query point.
+
+    Output
+    h       [lon, lat] on great circle closest to query point.
+    d       Perpendicular distance between great circle and point.
+    m       [lon, lat] of mirror image point.
+    '''
+
+    # Ellipsoid.
+    ell     = Geodesic.WGS84
+
+    # ab    Geodesic from A to B.
+    # az_ab Azimuth of geodesic from A to B.
+    ab      = ell.Inverse(a[1], a[0], b[1], b[0])
+    az_ab   = ab['azi1']
+
+    # ap    Geodesic from A to P.
+    # az_ap Azimuth of geodesic from A to P.
+    # d_p   Distance between A and P along geodesic.
+    ap      = ell.Inverse(a[1], a[0], p[1], p[0])
+    az_ap   = ap['azi1']
+    d_ap    = ap['s12']
+
+    # Point M is the image of P on the other side of line AB.
+    # az_am Azimuth of geodesic from A to M.
+    # am    Geodesic from A to M.
+    # m     [lon, lat] of point M.
+    az_am   = az_ab + (az_ab - az_ap)
+    am      = ell.Direct(a[1], a[0], az_am, d_ap)
+    m       = np.array([am['lon2'], am['lat2']])
+
+    # Point H is the half-way between P and M along a great circle.
+    # pm    Geodesic from P to M.
+    # d     Half of the distance from P to M.
+    # ph    Geodesic from P to H.
+    # h     [lon, lat] of point H.
+    pm      = ell.Inverse(p[1], p[0], m[1], m[0])
+    d       = pm['s12']/2.0
+    ph      = ell.Direct(p[1], p[0], pm['azi1'], d)
+    h       = np.array([ph['lon2'], ph['lat2']])
+
+    return h, d, m
+
+def test():
+
+    import  matplotlib.pyplot       as      plt
+    from    mpl_toolkits.basemap    import  Basemap
+
+    a   = np.array([ 80.0,  30.0])
+    b   = np.array([100.0,  50.0])
+
+    p   = np.array([ 70.0,  45.0])
+
+    h, d, m = dist_from_great_circle_to_point(a, b, p)
+
+    fig = plt.figure()
+    ax  = plt.gca()
+    bm  = Basemap(  projection  = 'ortho',
+                    lat_0       = 35.0,
+                    lon_0       = 85.0)
+    bm.drawcoastlines(linewidth=0.25)
+
+    points          = [a, b, p, m, h]
+    point_names     = ['A', 'B', 'P', 'M', 'H']
+    point_colors    = ['k', 'k', 'k', 'r', 'r']
+    point_label_xy  = [(-10, -10), (10, 10), (-10, 5), (10, -5), (10, -3)]
+
+    lines           = [[a, b], [p, m]]
+    line_colors     = ['k', 'r']
+
+    for point, color, name, label_xy in zip(
+            points, point_colors, point_names, point_label_xy):
+
+        bm.scatter( point[0], point[1],
+                    color       = color,
+                    marker      = '.',
+                    latlon      = True)
+
+        plt.annotate(   name,
+                        xy          = bm(point[0], point[1]),
+                        xycoords    = 'data',
+                        xytext      = label_xy,
+                        textcoords  = 'offset points',
+                        color       = color)
+
+    for line, color in zip(lines, line_colors):
+
+        bm.drawgreatcircle(
+                    line[0][0], line[0][1],
+                    line[1][0], line[1][1],
+                    color = color)
+
+    plt.show()
+
+if __name__ == '__main__':
+
+    test()
--- libraries/GeographicLib/distance-between-great-circle-and-point.py
+++ libraries/GeographicLib/distance-between-great-circle-and-point.py
@@ -17,22 +17,49 @@
 # 
 # print(f"Shortest distance to the great circle: {cross_track_distance / 1000:.2f} km")
 # 
+# ------------------------------------------
 # 
+# from geographiclib.geodesic import Geodesic
+# 
+# point1 = (37.7749, -122.4194)  # San Francisco, CA
+# point2 = (40.7128, -74.0060)   # New York, NY
+# point3 = (41.8781, -87.6298)   # Chicago, IL
+# 
+# geod = Geodesic.WGS84
+# 
+# # Calculate the geodesic line between point1 and point2
+# line = geod.InverseLine(point1[0], point1[1], point2[0], point2[1])
+# 
+# # Calculate the projection of point3 onto the line
+# projection = line.Position(line.ClosestPoint(point3[0], point3[1])['s12'])
+# 
+# # Calculate the distance between the original point3 and its projection on the line
+# cross_track_distance = geod.Inverse(point3[0], point3[1], projection['lat2'], projection['lon2'])['s12']
+# 
+# print(f"Shortest distance to the great circle: {cross_track_distance / 1000:.2f} km")
+
 from geographiclib.geodesic import Geodesic
+
+def cross_track_distance(point1, point2, point3):
+    geod = Geodesic.WGS84
+
+    # Calculate the initial azimuth and distance from point1 to point2
+    g = geod.Inverse(point1[0], point1[1], point2[0], point2[1])
+
+    # Calculate the initial azimuth and distance from point1 to point3
+    h = geod.Inverse(point1[0], point1[1], point3[0], point3[1])
+
+    # Calculate the cross-track distance using the spherical Earth formula
+    cross_track_dist = geod.CircleRadius(h['lat1']) * \
+                       abs(geod.SinAzimuthDifference(g['azi1'], h['azi1']) * geod.Sin(h['a12']))
+
+    return cross_track_dist
 
 point1 = (37.7749, -122.4194)  # San Francisco, CA
 point2 = (40.7128, -74.0060)   # New York, NY
 point3 = (41.8781, -87.6298)   # Chicago, IL
 
-geod = Geodesic.WGS84
+# Calculate the shortest distance from the great circle to the given point (Chicago)
+shortest_distance = cross_track_distance(point1, point2, point3)
 
-# Calculate the geodesic line between point1 and point2
-line = geod.InverseLine(point1[0], point1[1], point2[0], point2[1])
-
-# Calculate the projection of point3 onto the line
-projection = line.Position(line.ClosestPoint(point3[0], point3[1])['s12'])
-
-# Calculate the distance between the original point3 and its projection on the line
-cross_track_distance = geod.Inverse(point3[0], point3[1], projection['lat2'], projection['lon2'])['s12']
-
-print(f"Shortest distance to the great circle: {cross_track_distance / 1000:.2f} km")
+print(f"Shortest distance to the great circle: {shortest_distance / 1000:.2f} km")
