7d43e6f0788a72c64be06eb7b1198dbbf06c0c6b
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2023-03-26 17:41:09 +0200

    .

--- libraries/Cartopy/coastlines-3.py
+++ libraries/Cartopy/coastlines-3.py
@@ -1,13 +1,23 @@
 #
 #   https://stackoverflow.com/questions/53195551/cartopy-plotting-points-incorrectly-with-orthographic-projection
 #
-import numpy as np
+# import numpy as np
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 
+# cities = {
+#     'London': (-0.1276, 51.5072),
+#     'Zurich': (8.5417, 47.3769),
+#     'Yakutsk': (129.6759, 62.0355),
+#     'Natal': (-35.2009, -5.7793),
+#     'Nuuk': (-51.7214, 64.1836)
+# }
+
 # create the lat/lon points
-lons = np.array([214.5, 2.7, 197.5])
-lats = np.array([35, 36, 37.])
+# lons = np.array([214.5, 2.7, 197.5])
+# lats = np.array([35, 36, 37.])
+lons = [214.5, 2.7, 197.5]
+lats = [35   , 36 , 37.  ]
 
 # create the projections
 ortho = ccrs.Orthographic(central_longitude=0, central_latitude=90)
