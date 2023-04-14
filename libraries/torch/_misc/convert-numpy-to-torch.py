3d138a7ffc34e0752cffa46a9b35a3fb0a093de8
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2023-04-14 16:37:57 +0200

    .

--- libraries/torch/_misc/convert-numpy-to-torch.py
+++ libraries/torch/_misc/convert-numpy-to-torch.py
@@ -2,11 +2,11 @@ import numpy
 import torch
 
 
-data_np         = np.array([
+data_np    = numpy.array([
                      [1, 2, 3],
                      [4, 5, 6]
                   ])
 
-data_torch = map(torch.tensor, data_np)
+data_torch = list(map(torch.tensor, [ data_np ] ))
 
 print(data_torch)
