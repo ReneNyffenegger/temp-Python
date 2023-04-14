2e0338a1b1167ffa4d0fda18b987d8ca90960d5e
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2023-04-14 10:10:40 +0200

    .

--- libraries/torch/types/nn/MSELoss_.py
+++ libraries/torch/types/nn/MSELoss_.py
@@ -3,11 +3,8 @@ import torch
 # Create an instance of MSELoss
 criterion = torch.nn.MSELoss()
 
-# Example predicted values
-predicted = torch.tensor([1.2, 2.5, 3.8, 4.1])
-
-# Example ground-truth values
-target = torch.tensor([1.0, 2.0, 4.0, 4.5])
+predicted = torch.tensor([1.2]) # Predicted values
+target    = torch.tensor([1.0]) # Ground-truth (i. e. target) values
 
 # Calculate the loss
 loss = criterion(predicted, target)
