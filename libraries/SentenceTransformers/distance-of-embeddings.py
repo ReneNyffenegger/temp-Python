cda2c37ade73834ab914a06145b0c1e12d3834d4
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2023-10-14 16:02:49 +0200

    .

--- libraries/SentenceTransformers/distance-of-embeddings.py
+++ libraries/SentenceTransformers/distance-of-embeddings.py
@@ -4,11 +4,21 @@ import numpy as np
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
 
-emb_1 = model.encode('The roses are red.')
-emb_2 = model.encode('Die Rosen sind rot.')
-emb_3 = model.encode('The roses are blue.')
-emb_4 = model.encode('Strawberries are red.')
+# emb_1 = model.encode('The roses are red.')
+# emb_2 = model.encode('Die Rosen sind rot.')
+# emb_3 = model.encode('The roses are blue.')
+# emb_4 = model.encode('Strawberries are red.')
+# 
+# emb_5 = model.encode('Despite their contrasting appearances, both the elegant swan glided gracefully on the serene lake and the sleek swan moved with finesse across the tranquil pond, conveying a remarkable sense of poise and beauty in their aquatic realm.')
+# emb_6 = model.encode('While one may observe the graceful swan gliding serenely on the tranquil lake and the sleek swan moving with finesse across the calm pond as distinct scenes, they both exude a striking and harmonious elegance in their aquatic habitat.')
+# 
+# emb_7 = model.encode('red'  )
+# emb_8 = model.encode('blue' )
+# emb_9 = model.encode('green')
 
+
+emb_1 = model.encode('I went up the mountain and shot a deer.')
+emb_2 = model.encode('I climbed up and killed an animal.')
 
 def dist(emb_a, emb_b):
 
@@ -28,5 +38,10 @@ def dist(emb_a, emb_b):
 
 
 dist(emb_1, emb_2)
-dist(emb_1, emb_3)
-dist(emb_1, emb_4)
+
+# dist(emb_1, emb_3)
+# dist(emb_1, emb_4)
+# 
+# dist(emb_5, emb_6)
+# dist(emb_7, emb_8)
+# dist(emb_7, emb_9)
