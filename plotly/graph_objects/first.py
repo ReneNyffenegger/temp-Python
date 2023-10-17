e2aeeebd3b03bd30f0e5a2863a339e7af65e6450
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2023-10-18 00:49:39 +0200

    .

---
+++ plotly/graph_objects/first.py
@@ -0,0 +1,26 @@
+import plotly.graph_objects as go
+import numpy as np
+
+# Generate 100 random x, y, z coordinates
+np.random.seed(42)  # for reproducibility
+x = np.random.rand(100)
+y = np.random.rand(100)
+z = np.random.rand(100)
+
+# Create a 3D scatter plot
+fig = go.Figure(data=[go.Scatter3d(
+    x=x,
+    y=y,
+    z=z,
+    mode='markers',
+    marker=dict(size=6, opacity=0.8)
+)])
+
+# Set axis labels
+fig.update_layout(scene=dict(
+        xaxis_title='X AXIS TITLE',
+        yaxis_title='Y AXIS TITLE',
+        zaxis_title='Z AXIS TITLE')
+)
+
+fig.show()
