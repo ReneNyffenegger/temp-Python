bd91ad70ef919b746a3f170b6e1e114d12331eb7
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2024-02-06 01:03:33 +0100

    .

--- libraries/evdev/test.py
+++ libraries/evdev/test.py
@@ -1,7 +1,8 @@
 import evdev
 
 # device = evdev.InputDevice('/dev/input/event8')
-device = evdev.InputDevice('/dev/input/event0')
+# device = evdev.InputDevice('/dev/input/event0')
+device = evdev.InputDevice('/dev/input/event16')
 print(device)
 
 for event in device.read_loop():
