354b39b7157f8d2d3e872a8514cb11c35eccdf87
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2024-03-01 21:53:48 +0100

    .

--- libraries/evdev/_create-venv.sh
+++ libraries/evdev/_create-venv.sh
@@ -4,3 +4,14 @@
 
   pip3 install evdev
 # pip3 install ansi-escapes
+
+
+#
+#      The intention of the following was to change group ownership of dev/uinput to «uinput»
+#      It didn't work.
+# #
+# # https://github.com/chrippa/ds4drv/issues/93
+# # 
+#   sudo sh -c 'echo SUBSYSTEM=="misc", KERNEL=="uinput", MODE="0660", GROUP="uinput"  > /etc/udev/rules.d/tq4-uinput.rules'
+#   sudo sh -c 'echo uinput > /etc/modules-load.d/uinput.conf'
+#   sudo sh -c 'udevadm control --reload-rules && udevadm trigger'
