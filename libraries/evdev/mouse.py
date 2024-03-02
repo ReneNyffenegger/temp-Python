046ef4090ce4d1369d7feda272ca05e2044d001b
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2024-03-02 09:23:41 +0100

    .

--- libraries/evdev/mouse.py
+++ libraries/evdev/mouse.py
@@ -1,5 +1,8 @@
+import asyncio
+
 import evdev
-print(evdev.InputDevice('/dev/input/event0'))
+
+# print(evdev.InputDevice('/dev/input/event0'))
 
 # print('<\n>'.join([evdev.InputDevice(devpath).name for devpath in evdev.list_devices()]))
 # quit()
@@ -9,23 +12,37 @@ print(evdev.InputDevice('/dev/input/event0'))
 #
 # Find device object for device with a given name
 #
-# devname = 'LITEON Technology USB Multimedia Keyboard'
-devname = 'USB OPTICAL MOUSE ' # note the final space
+nm_kb = 'LITEON Technology USB Multimedia Keyboard'
+nm_ms = 'USB OPTICAL MOUSE ' # note the final space
 
-devobjs = [ devobj for devobj  in [ evdev.InputDevice(devpath) 
-                   for devpath in evdev.list_devices() ]
-              if devobj.name == devname
-          ]
+dv_kb = [ devobj for devobj  in [ evdev.InputDevice(devpath) for devpath in evdev.list_devices() ] if devobj.name == nm_kb ][0]
+dv_ms = [ devobj for devobj  in [ evdev.InputDevice(devpath) for devpath in evdev.list_devices() ] if devobj.name == nm_ms ][0]
 
-if devobjs == []:
-   print('no device found')
-   quit()
+# print(type(dv_kb))
+# print(type(dv_ms))
 
-devobj=devobjs[0]
+print(f'Keyboard at {dv_kb.path}')
+print(f'Mouse    at {dv_ms.path}')
 
-for ev in devobj.read_loop():
-    if ev.type == evdev.ecodes.EV_KEY: #  and ev.value == 1:
-#      print(evdev.categorize(ev))
-#      print(ev.code)
-#      print('y')
-       print(evdev.ecodes.BTN[ev.code])
+async def print_events(device):
+    async for event in device.async_read_loop():
+        print(device.path, evdev.categorize(event), sep=': ')
+
+for dev in dv_ms, dv_kb:
+    asyncio.ensure_future(print_events(dev))
+
+loop = asyncio.get_event_loop()
+loop.run_forever()
+
+# if devobjs == []:
+#    print('no device found')
+#    quit()
+# 
+# devobj=devobjs[0]
+# 
+# for ev in devobj.read_loop():
+#     if ev.type == evdev.ecodes.EV_KEY: #  and ev.value == 1:
+# #      print(evdev.categorize(ev))
+# #      print(ev.code)
+# #      print('y')
+#        print(evdev.ecodes.BTN[ev.code])
