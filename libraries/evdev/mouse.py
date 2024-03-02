3a5f271d0107e6474e874a65f619d1ce7d3e5592
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2024-03-02 10:40:17 +0100

    .

--- libraries/evdev/mouse.py
+++ libraries/evdev/mouse.py
@@ -32,14 +32,71 @@ print(f'Mouse    at {dv_ms.path}')
 
 atexit.register(dv_kb.ungrab)  # Don't forget to ungrab the keyboard on exit!
 dv_kb.grab()                   # Grab, i.e. prevent the keyboard from emitting original events.
-# left_alt_suppressed = False
+left_alt_suppressed = False
+
+# Create a new keyboard mimicking the original one.
+ui = evdev.UInput.from_device(dv_kb, name='kbdremap') 
 
 async def handle_events(dev): # {{{
+    global left_alt_suppressed
+    global ui
+
     async for ev in dev.async_read_loop():
-        if ev.type == evdev.ecodes.EV_KEY:
-           print(dev.path, evdev.categorize(ev), sep=': ')
-           if ev.code in [evdev.ecodes.BTN_LEFT, evdev.ecodes.BTN_MIDDLE, evdev.ecodes.BTN_RIGHT]:
-              print('btn')
+#       if ev.type == evdev.ecodes.EV_KEY:
+#          print(dev.path, evdev.categorize(ev), sep=': ')
+#          if ev.code in [evdev.ecodes.BTN_LEFT, evdev.ecodes.BTN_MIDDLE, evdev.ecodes.BTN_RIGHT]:
+#             print('btn')
+        if ev.type == evdev.ecodes.EV_KEY:  # Process key events.
+        
+#          print(kbd.active_keys())
+
+           if   ev.code == evdev.ecodes.KEY_PAUSE and ev.value == 1:
+              # Exit on pressing PAUSE.
+              # Useful if that is your only keyboard. =)
+              # Also if you bind that script to PAUSE, it'll be a toggle.
+                break
+
+
+           if    ev.code == evdev.ecodes.KEY_LEFTALT and ev.value == 1:
+                 left_alt_suppressed = True
+
+           elif  left_alt_suppressed and ev.code == evdev.ecodes.KEY_SEMICOLON and ev.value == 1:
+                 left_alt_suppressed = False
+                 write_hex([evdev.ecodes.KEY_F, evdev.ecodes.KEY_6]) # ö
+
+           elif  left_alt_suppressed and ev.code == evdev.ecodes.KEY_APOSTROPHE and ev.value == 1:
+                 left_alt_suppressed = False
+                 write_hex([evdev.ecodes.KEY_E, evdev.ecodes.KEY_4]) # ä
+          
+           elif  left_alt_suppressed and ev.code == evdev.ecodes.KEY_LEFTBRACE and ev.value == 1:
+                 left_alt_suppressed = False
+                 write_hex([evdev.ecodes.KEY_F, evdev.ecodes.KEY_C]) # ü
+
+           elif ev.code == evdev.ecodes.KEY_ESC:
+                ui.write(evdev.ecodes.EV_KEY, evdev.ecodes.KEY_CAPSLOCK, ev.value)
+
+           elif ev.code == evdev.ecodes.KEY_CAPSLOCK:
+                ui.write(evdev.ecodes.EV_KEY, evdev.ecodes.KEY_ESC, ev.value)
+
+           elif ev.code == evdev.ecodes.KEY_RIGHTMETA:
+                ui.write(evdev.ecodes.EV_KEY, evdev.ecodes.KEY_RIGHTCTRL, ev.value)
+
+           elif ev.code == evdev.ecodes.KEY_LEFTMETA:
+                ui.write(evdev.ecodes.EV_KEY, evdev.ecodes.KEY_LEFTCTRL, ev.value)
+
+           elif left_alt_suppressed:
+                ui.write(evdev.ecodes.EV_KEY, evdev.ecodes.KEY_LEFTALT, 1)
+                ui.write(evdev.ecodes.EV_KEY, ev.code, ev.value)
+                left_alt_suppressed = False
+                   
+#          elif evdev.ecodes.KEY_LEFTALT in kbd.active_keys() and evdev.ecodes.KEY_SEMICOLON in kbd.active_keys():
+
+           else:
+                ui.write(evdev.ecodes.EV_KEY, ev.code, ev.value)
+
+        else:
+          # Passthrough other events unmodified (e.g. SYNs).
+            ui.write(ev.type, ev.code, ev.value)
 # }}}
 
 for dev in dv_ms, dv_kb:
@@ -49,6 +106,23 @@ loop = asyncio.get_event_loop()
 loop.run_forever()
 
 # if devobjs == []:
+def write_hex(keys): # {{{
+
+    ui.write(evdev.ecodes.EV_KEY, evdev.ecodes.KEY_LEFTCTRL , 1)
+    ui.write(evdev.ecodes.EV_KEY, evdev.ecodes.KEY_LEFTSHIFT, 1)
+    ui.write(evdev.ecodes.EV_KEY, evdev.ecodes.KEY_U        , 1)
+    ui.write(evdev.ecodes.EV_KEY, evdev.ecodes.KEY_U        , 0)
+    ui.write(evdev.ecodes.EV_KEY, evdev.ecodes.KEY_LEFTCTRL , 0)
+    ui.write(evdev.ecodes.EV_KEY, evdev.ecodes.KEY_LEFTSHIFT, 0)
+
+    for key in keys:
+        ui.write(evdev.ecodes.EV_KEY, key, 1) # https://www.utf8-zeichentabelle.de/unicode-utf8-table.pl?names=-&unicodeinhtml=hex
+        ui.write(evdev.ecodes.EV_KEY, key, 0)
+
+    ui.write(evdev.ecodes.EV_KEY, evdev.ecodes.KEY_SPACE    , 1)
+    ui.write(evdev.ecodes.EV_KEY, evdev.ecodes.KEY_SPACE    , 0)
+    
+# }}}
 #    print('no device found')
 #    quit()
 # 
