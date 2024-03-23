2b96d682c87ad047957af12445955970df45f9c1
René Nyffenegger <mail@renenyffenegger.ch>
2024-03-23 11:19:03 +0100

    .

--- libraries/kraken/readme.txt
+++ libraries/kraken/readme.txt
@@ -5,3 +5,17 @@ Kraken ist eine weiterentikicklung der OCR-Software OCRopus, die für Hand- und 
 Kraken is related to eScriptorium
   einer Plattform zur manuellen oder automatisierten Segmentierung und
   Texterkennung von historischen Handschriften und Drucken. 
+
+  eScriptorium provides a user-friendly interface for annotating data, training
+  models, and inference (but also much more).
+
+OCR general notes
+ 
+   - high quality scans
+       - 300 dpi+ 
+       - lossless format (tiff, png) -> extract images from pdf with pdftocairo or pdfimages
+
+   - Only features that are found in the training data can later be recognized,
+
+Scantailor https://github.com/scantailor/scantailor 
+   A fairly user-friendly software for semi-automatic batch processing of image scans
--- libraries/kraken/script.py
+++ libraries/kraken/script.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
 
-#
-#   Currently, the script only tests if a python script
-#   ca import from kraken.
-#
+from PIL    import Image
+from kraken import binarization
+
+im = Image.open('/home/rene/github/temp/scans-for-OCR-tests/mine/tweet-177087971404902428.jpg')
+bw_im = binarization.nlbin(im)
+# type(bw_im) -> PIL.Image.Image
+bw_im.save('tweet.png')
+
+# -------------------------------------------------------------
+
 from kraken import pageseg
+seg = pageseg.segment(bw_im)
+# type(seg) -> kraken.containers.Segmentation
+
+print(seg.text_direction)
+
+print(type(seg.lines)) # list
+print(type(seg.lines[0])) kraken.containers.BBoxLine
+print(len(seg.lines))
+
+
+# -------------------------------------------------------------
+
+from kraken.lib import models
