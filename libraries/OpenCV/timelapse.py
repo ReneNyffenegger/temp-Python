8d737f342a670e7bcc75b5fb5c9edff3f1009109
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2024-03-25 14:50:52 +0100

    .

---
+++ libraries/OpenCV/timelapse.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+
+#
+#   After capturing images, create movie like so:
+#      ffmpeg -framerate 30 -pattern_type glob -i '*.jpg' -c:v libx264 -pix_fmt yuv420p out.mp4
+#
+
+import cv2
+import time
+
+cam = cv2.VideoCapture(0)
+
+cv2.namedWindow('frame-out')
+cnt = 0
+
+while True:
+
+      ret, frame = cam.read()  #read frame
+      if not ret:
+         break
+
+      cv2.imshow('frame-out', frame)
+
+      keyPressed = cv2.waitKey(1)
+
+      if   keyPressed % 256 == 27:
+         # ESC pressed: exit loop
+
+           break
+
+t = time.time()
+for cnt in range(100):
+
+      ret, frame = cam.read()
+
+      if not ret:
+         break
+
+      cv2.imshow('frame-out', frame)
+
+      img_name = f'timelapse-{cnt:04d}.jpg'
+      cv2.imwrite(img_name, frame)
+      print(f'{img_name} written')
+
+      time.sleep(10)
+
+
+cam.release()
+cv2.destroyAllWindows()
+
