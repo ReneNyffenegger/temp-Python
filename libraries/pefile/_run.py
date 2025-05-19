794fee264ef88508113891e0c0b7d8f4b971810b
René Nyffenegger <mail@renenyffenegger.ch>
2025-05-19 14:44:23 +0200

    .

--- libraries/pefile/_run.py
+++ libraries/pefile/_run.py
@@ -4,11 +4,19 @@ import pefile
 
 from pathlib import Path
 
-directory = Path('shobj')
-for dll_file in directory.rglob('*.dll'):
+def print_exports(pe):
+    for entry in pe.DIRECTORY_ENTRY_EXPORT.symbols:
+        print('  ' + entry.name.decode('utf-8'))
+
+if False:
+ # directory = Path('shobj')
+ directory = Path('IDM')
+ for dll_file in directory.rglob('*.dll'):
 
     print(dll_file)
     pe = pefile.PE(dll_file)
+    print_exports(pe)
 
-    for entry in pe.DIRECTORY_ENTRY_EXPORT.symbols:
-        print('  ' + entry.name.decode('utf-8'))
+
+pe = pefile.PE('IDM/sfl400as.dll')
+print_exports(pe)
