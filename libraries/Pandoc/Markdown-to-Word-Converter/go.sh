0c329145e07e43ce40db34cf4919a43d363fb3b7
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2026-08-02 14:20:48 +0200

    .

---
+++ libraries/Pandoc/Markdown-to-Word-Converter/go.sh
@@ -0,0 +1,9 @@
+sudo apt install -y pandoc
+
+git clone https://github.com/michellepace/markdown-to-word.git
+cd markdown-to-word # Move into repostory
+
+# venv
+# . py-virt-env/bin/activate
+
+pip install -r requirements.txt
---
+++ libraries/Pandoc/Markdown-to-Word-Converter/readme.txt
@@ -0,0 +1,1 @@
+https://github.com/michellepace/util-markdown-to-word
